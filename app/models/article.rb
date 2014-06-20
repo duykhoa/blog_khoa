c2a84cff7f9afcf83aa3f6d8fb79e394a97b1d20
Duy Khoa <duykhoa12t@gmail.com>
2014-06-20 23:42:50 +0700

    Improve UI for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,10 @@ class Article < ActiveRecord::Base
   has_attached_file(
     :feature_image,
     styles:
-      { :medium => "400x400>", :thumb => "100x100>" },
+      { :medium => "450x450>", :thumb => "100x100>" },
     default_url: "/images/:style/missing.png")
 
   validates_attachment_content_type :feature_image, :content_type => /\Aimage\/.*\Z/
+
+  default_scope lambda { order(created_at: :desc) }
 end
