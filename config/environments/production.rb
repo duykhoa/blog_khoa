a95b106dd1afc389bc67742ca02bd20db72563a8
Duy Khoa <duykhoa12t@gmail.com>
2014-06-21 11:18:06 +0700

    Precompile assets in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.precompile += [ /.*ckeditor.js/, /.*config.js/, /.*cs.js/, /.*editor.css/, /.*plugin.js/, /.*en.js/, /.*contents.css/, /.*default.js/ ]
 end
