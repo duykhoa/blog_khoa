8f32ac1c3cc609cbaf5394c580761d111967f2a2
Duy Khoa <duykhoa12t@gmail.com>
2014-06-28 09:29:41 +0700

    Improve UI for pagination, remove footer bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,12 @@ module ApplicationHelper
     DateTime.parse(date).strftime('%B %-d, %Y')
   end
 
+  def page_params(params)
+    params.key?(:page) ? params.fetch(:page).to_i : 1
+  end
+
   def pagination_previous(articles, page)
-    if page && page.to_i > 1
+    if page && page > 1
       content_tag :div, class: 'previous' do
         link_to 'Previous Page', url_for(page: page.to_i - 1)
       end
@@ -12,7 +16,7 @@ module ApplicationHelper
   end
 
   def pagination_next(articles, page)
-    if !page || 0 < page.to_i < total_page(articles.total_count)
+    if page.in?(0...total_page(articles.total_count))
       content_tag :div, class: 'next' do
         link_to 'Next Page', url_for(page: page.to_i + 1)
       end
