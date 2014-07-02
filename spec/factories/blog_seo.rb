71e735919c6eefeee8e375467a0d1d3dee1700ae
Duy Khoa <duykhoa12t@gmail.com>
2014-07-03 00:31:23 +0700

    after build for blog_seo script

--- spec/factories/blog_seo.rb
+++ spec/factories/blog_seo.rb
@@ -1,7 +1,15 @@
 FactoryGirl.define do
   factory :blog_seo do
+    ignore do
+      value_nil false
+    end
+
     sequence :value do |n|
       "value#{n}"
+    end
+
+    after(:build) do |blog_seo, evalutor|
+      blog_seo.value = nil if evalutor.value_nil
     end
   end
 end
