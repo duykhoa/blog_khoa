9a28667bc7b023064e8c58adf459da3f9706839c
Duy Khoa <duykhoa12t@gmail.com>
2014-07-02 23:42:47 +0700

    add blog_setting_spec

--- spec/models/blog_setting_spec.rb
+++ spec/models/blog_setting_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe BlogSetting, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe BlogSetting, :type => :model do
+  it { should validate_uniqueness_of(:key) }
+
+  describe ".settings" do
+    before(:each) do
+      BlogSetting.class_eval do
+        @@setting = nil
+      end
+
+      settings = double('BlogSetting')
+      allow(BlogSetting).to receive(:all).and_return(settings)
+      allow(settings).to receive(:inject).and_return({})
+    end
+
+    it "hits database for the first time" do
+      expect(BlogSetting).to receive(:all)
+      expect(BlogSetting.settings).to eq({})
+    end
+
+    it "does not hit database after that" do
+      expect(BlogSetting).to receive(:all).once
+
+      BlogSetting.settings
+      BlogSetting.settings
+    end
+  end
 end
