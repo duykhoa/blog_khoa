4b64dc9a5b519e3d3dcdd1093b980f6cccc4a385
Duy Khoa <duykhoa12t@gmail.com>
2014-07-20 08:29:07 +0700

    Fix path for error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,13 @@ class ApplicationController < ActionController::Base
 
   layout :theme_layout
 
-  unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, with: lambda { |exception| render_error 500, exception }
-    rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
-  end
+  rescue_from Exception, with: lambda { |exception| render_error 500, exception }
+  rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
 
   private
   def render_error(status, exception)
     respond_to do |format|
-      format.html { render template: "errors/error_#{status}", layout: 'layouts/application', status: status }
+      format.html { render template: File.join(view_path, "errors/error_#{status}"), layout: File.join(view_path, 'layouts/application'), status: status }
       format.all { render nothing: true, status: status }
     end
   end
