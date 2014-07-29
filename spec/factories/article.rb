16710211179dbbc2bfb435423db80e6e292b76a3
Duy Khoa <duykhoa12t@gmail.com>
2014-07-29 07:15:33 +0700

    Update rspec and improve search term

--- spec/factories/article.rb
+++ spec/factories/article.rb
@@ -3,6 +3,12 @@ FactoryGirl.define do
     sequence :title do |n|
       "Article #{n}"
     end
+    sequence :short_content do |n|
+      "Short Content #{n}"
+    end
+    sequence :content do |n|
+      "Content #{n}"
+    end
     association :category
   end
 end
--- spec/modules/params_ext_spec.rb
+++ spec/modules/params_ext_spec.rb
@@ -4,7 +4,7 @@ shared_examples_for "params_ext" do
   describe "#sanitize_params" do
     it "gsub all non word characters" do
       expect(described_class.new.send(:sanitize_params, "this^is*&|990params"))
-        .to eq("this-is---990params")
+        .to eq("this-is-star-and-990params")
     end
 
     it "retuns '-' if no params passed in" do
