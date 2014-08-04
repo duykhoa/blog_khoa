409c92486b5edf08e306045da341a10422086572
Duy Khoa <duykhoa12t@gmail.com>
2014-08-05 06:16:24 +0700

    fix wrong string syntax in capistrano script

--- config/deploy/phuongbui.me/production.rb
+++ config/deploy/phuongbui.me/production.rb
@@ -1,5 +1,5 @@
 set :deploy_to, '/home/deploy/blog_khoa/'
 set :rails_env, :production
-server 128.199.137.128, user: deploy, roles: %w{web app db}
+server '128.199.137.128', user: deploy, roles: %w{web app db}
 set :rvm_custom_path, '/usr/local/rvm'  # only needed if not detected
 
--- config/deploy/trantat.com/production.rb
+++ config/deploy/trantat.com/production.rb
@@ -1,5 +1,5 @@
 set :deploy_to, '/home/deploy/blog_khoa_trantat.com/'
 set :rails_env, :production
-server 128.199.137.128, deploy, roles: %w{web app db}
+server '128.199.137.128', deploy, roles: %w{web app db}
 set :rvm_custom_path, '/usr/local/rvm'  # only needed if not detected
 
