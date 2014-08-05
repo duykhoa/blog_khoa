3e9ec281fac631cb9b2cfef1a84a475da00f6b41
Duy Khoa <duykhoa12t@gmail.com>
2014-08-06 00:31:45 +0700

    Ignore deploy server config file

--- config/deploy/phuongbui_me/production.rb
+++
@@ -1,4 +0,0 @@
-set :rvm_custom_path, '/usr/local/rvm'  # only needed if not detected
-set :deploy_to, '/home/deploy/blog_khoa/'
-set :rails_env, :production
-server '128.199.137.128', user: 'deploy', roles: %w{web app db}
--- config/deploy/trantat_com/production.rb
+++
@@ -1,3 +0,0 @@
-set :deploy_to, '/home/deploy/blog_khoa_trantat.com/'
-set :rails_env, :production
-server '128.199.137.128', 'deploy', roles: %w{web app db}
