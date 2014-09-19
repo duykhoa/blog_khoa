2c563cdfa5b65c47995b5a879ded4e94a5182047
Duy Khoa <duykhoa12t@gmail.com>
2014-09-19 22:38:25 +0700

    avatar for about me section optimization

--- app/models/about_me.rb
+++ app/models/about_me.rb
@@ -2,7 +2,7 @@ class AboutMe < ActiveRecord::Base
   has_attached_file(
     :avatar,
     styles:
-      { :w70 => "70x70>" },
+      { :thumb => "120x120#" },
     default_url: "/images/missing.png")
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
