c8ce7324e6c8b5b7e232a09c084d8f6c03a2b4db
Duy Khoa <duykhoa12t@gmail.com>
2014-11-04 23:12:33 +0700

    improve comment UI and add ajax request to create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,16 @@
 class CommentsController < ApplicationController
+  before_action :comment_params
+
   def create
+    comment = Comment.new(comment_params)
+    comment.save
+
     render json: {status: :success}
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment, :commentable_id, :commentable_type)
   end
 end
