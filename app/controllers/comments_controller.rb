5ca99ef0a758d744e2e907a82637072834021047
Duy Khoa <duykhoa12t@gmail.com>
2014-11-05 00:23:45 +0700

    remove ajax request for comment and add user_id to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,21 @@
 class CommentsController < ApplicationController
   before_action :comment_params
+  before_action :set_article
 
   def create
     comment = Comment.new(comment_params)
     comment.save
 
-    render json: {status: :success}
+    redirect_to @article
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:comment, :commentable_id, :commentable_type)
+    params.require(:comment).permit(:comment, :commentable_id, :commentable_type, :user_id)
+  end
+
+  def set_article
+    @article = Article.find params.fetch(:comment).fetch(:commentable_id)
   end
 end
