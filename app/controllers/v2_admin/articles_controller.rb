f9213a3ab08f981b1d87ed29fcaf89027f55a4bc
Duy Khoa <duykhoa12t@gmail.com>
2014-11-21 19:10:44 +0700

    add destroy action for v2_admin/articles controller

--- app/controllers/v2_admin/articles_controller.rb
+++ app/controllers/v2_admin/articles_controller.rb
@@ -1,8 +1,26 @@
 class V2Admin::ArticlesController < V2Admin::HomesController
 
+  before_action :set_article, only: [:show, :edit, :destroy]
+
   include ParamsExt
 
   def index
     @articles = Article.search(index_params(params))
+  end
+
+  def destroy
+    status = @article.destroy ? { success: true, action: :destroy } : { success: false, action: destroy }
+
+    redirect_to v2_admin_articles_path, flash: status
+  end
+
+  private
+
+  def set_article
+    @article = Article.find article_param[:id]
+  end
+
+  def article_param
+    params.permit :id
   end
 end
