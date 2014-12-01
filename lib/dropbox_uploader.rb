de603136e2c7a17be4fb77c563bf37a8fb142667
Duy Khoa <duykhoa12t@gmail.com>
2014-12-01 21:51:49 +0700

    Basic module for dropbox uploader

---
+++ lib/dropbox_uploader.rb
@@ -0,0 +1,15 @@
+class DropboxUploader
+  #Dropbox::API::Client.new(token: Settings.dropbox.access_token, secret: Settings.dropbox.access_secret)
+
+  attr_reader :uploader
+
+  def initialize
+    @uploader = Dropbox::API::Client.new(token: Settings.dropbox.access_token, secret: Settings.dropbox.access_secret)
+  end
+
+  def upload(file)
+    raise "NotAFile" unless file.is_a?(File)
+
+    @uploader.chunked_upload(SecureRandom.hex(20), file).direct_url.url rescue nil
+  end
+end
