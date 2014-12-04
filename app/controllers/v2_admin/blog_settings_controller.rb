5bf2dabdf6d3016ddd6658f2239974fea862435d
Duy Khoa <duykhoa12t@gmail.com>
2014-12-04 22:37:12 +0700

    authenticate user and fix the broken value for BlogSetting

--- app/controllers/v2_admin/blog_settings_controller.rb
+++ app/controllers/v2_admin/blog_settings_controller.rb
@@ -1,4 +1,5 @@
 class V2Admin::BlogSettingsController < ApplicationController
+  before_filter :authenticate_user!
   layout 'admin'
 
   def index
@@ -6,6 +7,7 @@ class V2Admin::BlogSettingsController < ApplicationController
 
   def update
     BlogSetting.bulk_update(blog_setting_params)
+    redirect_to v2_admin_blog_settings_path
   end
 
   private
@@ -14,7 +16,7 @@ class V2Admin::BlogSettingsController < ApplicationController
     params
       .require(:blog_setting)
       .permit(
-        :blog_title, :blog_description, :facebook_image_url, :fb_description,
+        :blog_title, :blog_description, :facebook_image_url, :facebook_description,
         :mailchimp_api_key, :mailchimp_list_id, :email_subscriber_text
       )
   end
