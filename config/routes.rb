53222baa939874f2270205f2b0f6b03967f07f46
Duy Khoa <duykhoa12t@gmail.com>
2014-12-06 14:05:19 +0700

    email subscribers layout

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :assets, only: :create
     resources :blog_settings, only: :index
     resources :categories, only: :index
+    resources :email_subscribers, only: :index
 
     patch 'blog_settings/updates' => 'blog_settings#update', as: :update_blog_settings
   end
