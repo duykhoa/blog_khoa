eb6381a76f8033728449dc9c300b135aa03aa28b
Duy Khoa <duykhoa12t@gmail.com>
2014-12-11 12:57:39 +0700

    remove category that is not listed in category_list params

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,12 +11,18 @@ class Category < ActiveRecord::Base
   end
 
   def self.update_or_create(category_list)
+    delete_category_not_in(category_list.values.map {|category| category[0]})
+
     category_list.each do |index, category_names|
       category = find_or_initialize_by(name: category_names[0])
       category.name = category_names[1]
       category.position = index
       category.save
     end
+  end
+
+  def self.delete_category_not_in(existed_categories)
+    where.not(name: existed_categories).delete_all
   end
 
   rails_admin do
