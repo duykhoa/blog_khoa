c372eefdc4f394b7e840740976fbc32b21d05464
Duy Khoa <duykhoa12t@gmail.com>
2015-03-14 20:56:45 +0700

    remove admin scope in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -20,12 +20,8 @@ Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   devise_for :users
 
-  namespace :admin do
-    resources :articles
-  end
-
   get 'about' => 'abouts#index'
-  get '/search(/category/:category_name)' => 'articles#search', as: :seo_search
+  get '/search(/category/:category_name)(/:query)' => 'articles#search', as: :seo_search
 
   get 'feed', to: 'articles#index', defaults: {format: 'rss'}, as: :feed
   get '/(page/:page)' => 'articles#index', as: :index_seo
